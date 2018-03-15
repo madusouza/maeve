893d9fce86c49f59e1e85905db286c5675480c4d
Madu Souza <ducieledesouza@gmail.com>
2018-03-15 10:19:43 -0300

    Inclusão de monitoramento do PJe 2º Grau e Correição Virtual

--- maeve.py
+++ maeve.py
@@ -16,10 +16,10 @@ arquivo_log = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'maeve.l
 logging.basicConfig(
     filename=arquivo_log,
     level=logging.NOTSET,
-    format='[%(asctime)s] [%(name)s:%(lineno)d] %(levelname)s - %(message)s',
+    format='[ %(asctime)s ] [ %(name)s:%(lineno)d - %(funcName)s() ] %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-handler = logging.handlers.RotatingFileHandler(arquivo_log, maxBytes=100, backupCount=5)
+handler = logging.handlers.RotatingFileHandler(arquivo_log, maxBytes=1000000, backupCount=5)
 logging.getLogger().addHandler(handler)
 
 
@@ -61,14 +61,14 @@ def pje1g():
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'usuario-logado')))
         end = time.time()
         tempo_login = end - start
-        zbx_enviar('PJe 1Grau','tempo_login',tempo_login)
-        logging.info('Tempo login: ' + str(tempo_login))
+        zbx_enviar('PJe 1Grau','tempo_login', tempo_login)
+        logging.info('Tempo login PJe 1Grau: ' + str(tempo_login))
 
     except Exception:
         zbx_enviar('PJe 1Grau', 'tempo_login', 0)
         zbx_enviar('PJe 1Grau', 'tempo_logout', 0)
         zbx_enviar('PJe 1Grau', 'tempo_pesquisa', 0)
-        logging.error("O login falhou.")
+        logging.error("Login PJe 1Grau falhou.")
         driver.quit()
 
     try:
@@ -99,11 +99,11 @@ def pje1g():
         end = time.time()
         tempo_pesquisa = end - start
         zbx_enviar('PJe 1Grau', 'tempo_pesquisa', tempo_pesquisa)
-        logging.info("Tempo pesquisa: " + str(tempo_pesquisa))
+        logging.info("Tempo pesquisa PJe 1Grau: " + str(tempo_pesquisa))
 
     except Exception:
         zbx_enviar('PJe 1Grau', 'tempo_pesquisa', 0)
-        logging.error("A pesquisa falhou.")
+        logging.error("Pesquisa PJe 1Grau falhou.")
 
     try:
         # Logout
@@ -114,12 +114,98 @@ def pje1g():
         end = time.time()
         tempo_logout = end - start
         zbx_enviar('PJe 1Grau', 'tempo_logout', tempo_logout)
-        logging.info('Tempo logout: ' + str(tempo_logout))
+        logging.info('Tempo logout PJe 1Grau: ' + str(tempo_logout))
         driver.quit()
 
     except Exception:
         zbx_enviar('PJe 1Grau', 'tempo_logout', 0)
-        logging.error("O logout falhou.")
+        logging.error("Logout PJe 1Grau falhou.")
+        driver.quit()
+
+
+def pje2g():
+    driver = setup()
+    wait = WebDriverWait(driver, 10)
+    config = configparser.ConfigParser()
+    config.read('maeve.conf')
+    usuario = config.get('PJe2G', 'usuario')
+    senha = config.get('PJe2G', 'senha')
+    url = config.get('PJe2G', 'url')
+
+    try:
+        # Login
+        start = time.time()
+        driver.get(url)
+        user = driver.find_element_by_id('username')
+        user.send_keys(usuario)
+        password = driver.find_element_by_id('password')
+        password.send_keys(senha)
+        password.submit()
+
+        # Verifica se logou
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'usuario-logado')))
+        end = time.time()
+        tempo_login = end - start
+        zbx_enviar('PJe 2Grau','tempo_login', tempo_login)
+        logging.info('Tempo login PJe 2Grau: ' + str(tempo_login))
+
+    except Exception:
+        zbx_enviar('PJe 2Grau', 'tempo_login', 0)
+        zbx_enviar('PJe 2Grau', 'tempo_logout', 0)
+        zbx_enviar('PJe 2Grau', 'tempo_pesquisa', 0)
+        logging.error("Login PJe 2Grau falhou.")
+        driver.quit()
+
+    try:
+        # Pesquisa
+        start = time.time()
+
+        # Navegação no menu para entrar na pesquisa
+        menu_processo = wait.until(EC.visibility_of_element_located((By.ID, '_1007_j_id70j_id71')))
+        ActionChains(driver).move_to_element(menu_processo).perform()
+        menu_pesquisar_n1 = wait.until(EC.visibility_of_element_located((By.ID, '_1008_j_id70j_id75')))
+        ActionChains(driver).move_to_element(menu_pesquisar_n1).perform()
+        menu_pesquisar_n2 = wait.until(EC.visibility_of_element_located((By.ID, '_1009_j_id70j_id76')))
+        ActionChains(driver).move_to_element(menu_pesquisar_n2).click().perform()
+        wait.until(EC.visibility_of_element_located((By.ID, 'fPP:consultaSearchFields_header')))
+
+
+        # Preenchimento do formulário de pesquisa
+        num_sequencial = driver.find_element_by_id('fPP:numeroProcesso:numeroSequencial')
+        num_sequencial.send_keys('0800754')
+        num_digito_verificador = driver.find_element_by_id('fPP:numeroProcesso:numeroDigitoVerificador')
+        num_digito_verificador.send_keys('89')
+        num_processo_ano = driver.find_element_by_id('fPP:numeroProcesso:Ano')
+        num_processo_ano.send_keys('2017')
+        num_orgao_justica = driver.find_element_by_id('fPP:numeroProcesso:NumeroOrgaoJustica')
+        num_orgao_justica.send_keys('5106')
+        submete_pesquisa = driver.find_element_by_id('fPP:searchProcessos')
+        submete_pesquisa.click()
+        wait.until(EC.visibility_of_element_located((By.ID, '0800754-89.2017.8.20.5106')))
+        end = time.time()
+        tempo_pesquisa = end - start
+        zbx_enviar('PJe 2Grau', 'tempo_pesquisa', tempo_pesquisa)
+        logging.info("Tempo pesquisa PJe 2Grau: " + str(tempo_pesquisa))
+
+    except Exception:
+        zbx_enviar('PJe 2Grau', 'tempo_pesquisa', 0)
+        logging.error("Pesquisa PJe 2Grau falhou.")
+
+    try:
+        # Logout
+        start = time.time()
+        logout = driver.find_element_by_id('desconectar')
+        logout.click()
+        wait.until(EC.visibility_of_element_located((By.ID, 'username')))
+        end = time.time()
+        tempo_logout = end - start
+        zbx_enviar('PJe 2Grau', 'tempo_logout', tempo_logout)
+        logging.info('Tempo logout PJe 2Grau: ' + str(tempo_logout))
+        driver.quit()
+
+    except Exception:
+        zbx_enviar('PJe 2Grau', 'tempo_logout', 0)
+        logging.error("Logout PJe 2Grau falhou.")
         driver.quit()
 
 
@@ -145,12 +231,12 @@ def correicao():
         end = time.time()
         tempo_login = end - start
         zbx_enviar('Correicao Virtual', 'tempo_login', tempo_login)
-        logging.info('Tempo login correição: ' + str(tempo_login))
+        logging.info('Tempo login Correição: ' + str(tempo_login))
 
     except Exception:
         zbx_enviar('Correicao Virtual', 'tempo_login', 0)
         zbx_enviar('Correicao Virtual', 'tempo_logout', 0)
-        logging.error("O login correicao falhou.")
+        logging.error("O login Correicao falhou.")
         driver.quit()
 
     try:
@@ -162,24 +248,25 @@ def correicao():
         end = time.time()
         tempo_logout = end - start
         zbx_enviar('Correicao Virtual', 'tempo_logout', tempo_logout)
-        logging.info('Tempo logout correicao: ' + str(tempo_logout))
+        logging.info('Tempo logout Correicao: ' + str(tempo_logout))
         driver.quit()
 
     except Exception:
         zbx_enviar('Correicao Virtual', 'tempo_logout', 0)
-        logging.error("O logout correicao falhou.")
+        logging.error("O logout Correicao falhou.")
         driver.quit()
 
 
 def main():
     try:
-        os.chdir(os.path.dirname(__file__))
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
         pje1g()
-        # correicao()
+        pje2g()
+        correicao()
         return 0
 
     except Exception:
-        logging.error("Não foi possível executar os testes.")
+        logging.error("Não foi possível executar os testes, verifique o log.")
         return 1
 
 
